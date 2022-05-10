adf62a7b3b898421266f8372130e887ccc4d8007
lrod <ballestax@gmail.com>
2022-05-10 14:35:36 -0500

    initial commit

---
+++ src/main/java/com/lrod/demospr/DemosprApplication.java
@@ -0,0 +1,13 @@
+package com.lrod.demospr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosprApplication.class, args);
+	}
+
+}
